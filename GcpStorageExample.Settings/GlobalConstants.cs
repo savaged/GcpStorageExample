d9dcaa8400413d0ca11bc002c949c89730420cee
savaged <david.savage@assure-consulting.co.uk>
2021-04-13 15:02:31 +0100

    trying on Windows 10

--- GcpStorageExample.Settings/GlobalConstants.cs
+++ GcpStorageExample.Settings/GlobalConstants.cs
@@ -5,6 +5,6 @@ namespace GcpStorageExample.Settings
     public static partial class GlobalConstants
     {
         public const string STORAGE_KEY = "storage-key/sample-key.json";
-        public const string LOCAL_WORKING_DIR = "~/tmp/";
+        public const string LOCAL_WORKING_DIR = "/tmp/";
     }
 }
